57d729cb9ee8809a233db3c162f62c96a08ff33a
KeerthiReddy520 <keerthireddybandi.kr@gmail.com>
2026-02-07 13:59:03 +0530

    Add files via upload

---
+++ relational.java
@@ -0,0 +1,14 @@
+class relational
+{
+    public static void main(String[] args)
+    {
+        int a=10,b=4;
+        System.out.println(a=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}

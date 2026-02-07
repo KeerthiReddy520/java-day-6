d9d19805d89f3925734c81d6795740a1530b3c0c
KeerthiReddy520 <keerthireddybandi.kr@gmail.com>
2026-02-07 14:58:34 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,29 @@
+class operators
+{
+    public static void main(String[] args)
+    {
+      int a=5,b=6;
+      System.out.println("ARTHAMATIC OPERATOR");
+      System.out.println(a+b);
+      System.out.println("LOGICAL OPERATOR");
+      if(a>0&&a%2==0)
+      {
+        System.out.println("a is positive and even");
+      }
+      else
+      {
+        System.out.println("a is not positive and even");
+      }
+      System.out.println("unary Operator");
+      System.out.println(++a);
+      System.out.println("RELATIONAL OPEATOR");
+      if(a>0)
+      {
+         System.out.println(" a is positive");
+      }
+       else
+      { 
+         System.out.println(" a is not positive");
+      }
+    }
+}

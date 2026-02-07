78d42b4d1fa1a19686cc137b20836578a85441be
KeerthiReddy520 <keerthireddybandi.kr@gmail.com>
2026-02-07 14:17:45 +0530

    Add files via upload

---
+++ logical.java
@@ -0,0 +1,15 @@
+class logical
+{
+    public static void main(String[] args)
+    {
+        int marks=45;
+        if(marks>=90&&marks<=100)
+        {
+            System.out.println("A grade");
+        }
+        else
+        {
+            System.out.println("fail");
+        }
+    }
+}
